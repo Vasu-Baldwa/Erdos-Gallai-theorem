2a710cb1b53d952f79d08edc75b505263a8c8f64
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-07-24 15:48:21 -0400

    Fixed spacing

--- HH.py
+++ HH.py
@@ -3,25 +3,25 @@
 
 
 def HavelHakimi(a):
-	while True:
-		#if all ints == 0
-		if a[0]== 0 and a[len(a)-1]== 0:
-			return True
-		element1 = a[0]
-		a = a[1:]
-		#Not enough elements remaining for the subtraction step (No simple graph exists).
-		if element1 > len(a):
-            print("Not enough elements remaining for the subtraction step")
-			return False
-		#Subtraction step
-		for i in range(v):
-			a[i]-= 1
-			#Negative number after subtraction
-			if a[i]<0:
-                print("Negative number after subtraction")
-                print("a[i]: " + str(a[i]))
-				return False
+    while True:
+    #if all ints == 0
+    if a[0]== 0 and a[len(a)-1]== 0:
+        return True
+    element1 = a[0]
+    a = a[1:]
+    #Not enough elements remaining for the subtraction step (No simple graph exists).
+    if element1 > len(a):
+        print("Not enough elements remaining for the subtraction step")
+        return False
+    #Subtraction step
+    for i in range(v):
+        a[i]-= 1
+        #Negative number after subtraction
+        if a[i]<0:
+            print("Negative number after subtraction")
+            print("a[i]: " + str(a[i]))
+            return False
 
 
-test = [3, 3, 3, 3]
+test = [6, 6, 5, 4, 3, 3, 1]
 print(HavelHakimi(test))
