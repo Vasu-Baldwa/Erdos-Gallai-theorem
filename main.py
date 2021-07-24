28a95f1f2862a03e302541191d8853f5c373c535
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2021-07-24 01:21:28 -0400

    Bug Fix1

--- main.py
+++ main.py
@@ -3,35 +3,52 @@ as the degree sequence of a finite simple graph on n vertices if and
 only if d_i + ... + d_n is even and sum {i=1} to k = d_i <= (k(k-1) + (sum {j = k+1} to n min(d_i,k))
 holds for every k in 1 <= k <= n. """
 
+def getRHS(k, n, inputList):
+    minVal = 0
+    d_j = 0
+    for j in range(k+1, n+1):
+        d_j = inputList[j-1]
+        minVal = minVal + min(d_j, k)
+    return minVal
+
 def edt(inputList):
-    listSum = 0
     n = len(inputList)
-    for i in inputList:
-        listSum = listSum + i
-    if (listSum % 2 == 0):
-        k = 0
+    listDone = 1
+    if (sum(inputList) % 2 == 0):
+        k = 1
         d_i = 0
         #LHS
-        for (i) in range(1, n+1):
-            minVal = 0
-            d_i = inputList[i-1]
+        for (i) in range(1, n):
+            d_i = d_i + inputList[i-1]
             #RHS
-            for (j) in range(1, n+1):
-                minVal = minVal + min(inputList[j-1], k)
-            if (d_i <= (k * (k-1) + minVal)):
-                #List Done check
-                if(i == n+1):
+            RHS = getRHS(k,n,inputList)
+            """print("d_i: " + str(d_i))
+            print("k: " + str(k))
+            print("k - 1: " + str(k - 1))
+            print("k * (k-1): " + str(k * (k-1)))
+            print("RHS: " + str(RHS))"""
+            if (d_i <= (k * (k-1) + RHS)):
+                if(listDone == n):
                     return True
             else:
-                print("Failed on a d_i <= check. d_i = " + str(d_i) + " and k = " + str(k))
+                print("Failed on a d_i check " + str(d_i) + " was not <= " + str((k * (k-1)) + RHS))
                 return False
-    print("Was not even")
-    return False
+            listDone = listDone + 1
+            k = k + 1
+            #print("###############")
+    else:
+        print("Was not even")
+        return False
+    print("Error")
+
 
 #Tests
-fail = [3,3,3,1]
+
+#print(getRHS(2,4,[3,3,3,1]) == 3)
+
+#fail = [3,3,3,1]
+#print(not edt(fail))
 pass1 = [3,2,2,1]
-pass2 = [4,3,3,2,2,2]
-print(not edt(fail))
 print(edt(pass1))
-print(edt(pass2))
+#pass2 = [4,3,3,2,2,2]
+#print(edt(pass2))
